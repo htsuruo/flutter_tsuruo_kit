0c37a0ae426c87cdc1a46e4e396f5af5b52d5950
tsuruo <tsuru.dev@gmail.com>
2021-10-03 16:51:06 +0900

    Rename Enum extention method for coming Dart 2.15 feature.

--- lib/extensions/enum.dart
+++ lib/extensions/enum.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/foundation.dart';
 
+// Dart 2.15で`name``byName`が言語レベルで入ってくるのでこれらExtensionは不要となる
+// 先んじて命名を揃えておく
+// ref. https://github.com/dart-lang/sdk/blob/dev/sdk/lib/core/enum.dart
 extension EnumX on Enum {
-  String get described => describeEnum(this);
+  String get name => describeEnum(this);
 }
 
 extension EnumsX<T extends Enum> on List<T> {
-  T from(String value) => firstWhere(
+  T byName(String value) => firstWhere(
         (e) => '$e'.split('.').last == value,
         orElse: () => throw AssertionError('Enum $T has not $value'),
       );
