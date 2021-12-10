58c0eb1d4871ddaf24d691a1bf9d68647dfd78ad
tsuruo <tsuru.dev@gmail.com>
2021-12-10 23:30:12 +0900

    Remove StateController extension

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -1,6 +1,5 @@
 export 'document_reference.dart';
 export 'navigator_delayed_push.dart';
 export 'navigator_state.dart';
-export 'state_controller.dart';
 export 'state_notifier.dart';
 export 'stream.dart';
