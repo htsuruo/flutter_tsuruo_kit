ee538dfff142a0cae711500e0f12168d9b876087
tsuruo <tsuru.dev@gmail.com>
2022-03-18 17:39:44 +0900

    Add byNameOrNull

---
+++ lib/extensions/enum.dart
@@ -0,0 +1,14 @@
+extension EnumByNameX<T extends Enum> on Iterable<T> {
+  // 該当のEnumが存在しない場合はnullを返す
+  T? byNameOrNull(String? name) {
+    try {
+      if (name == null) {
+        return null;
+      }
+      return byName(name);
+      // ignore: avoid_catches_without_on_clauses
+    } catch (_) {
+      return null;
+    }
+  }
+}
--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -1,4 +1,5 @@
 export 'document_reference.dart';
+export 'enum.dart';
 export 'navigator_delayed_push.dart';
 export 'navigator_state.dart';
 export 'state_notifier.dart';
