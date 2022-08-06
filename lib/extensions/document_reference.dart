ffc23b3f7572859b160591cea6b11ba9173ecead
tsuruo <tsuru.dev@gmail.com>
2022-08-06 20:01:05 +0900

    Remove JsonMap because of support to json_converter_helper
    - https://pub.dev/packages/json_converter_helper

--- lib/extensions/document_reference.dart
+++ lib/extensions/document_reference.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:tsuruo_kit/tsuruo_kit.dart';
 
 extension DocumentReferenceX<E> on DocumentReference<E> {
   // `withConverter`されたDocumentReferenceを元のDocumentReferenceに戻す
@@ -7,5 +6,6 @@ extension DocumentReferenceX<E> on DocumentReference<E> {
   // クエリで扱う`_JsonDocumentReference`ではなくなるため、クエリ判定でできなくなる
   // whereクエリを利用する場合は一度元のDocumentReference（`_JsonDocumentReference`）に戻す必要がある
   // ref. https://github.com/39works/mytradeApp/pull/239#issuecomment-840568544
-  DocumentReference<JsonMap> get raw => FirebaseFirestore.instance.doc(path);
+  DocumentReference<Map<String, dynamic>> get raw =>
+      FirebaseFirestore.instance.doc(path);
 }
--- lib/tsuruo_kit.dart
+++ lib/tsuruo_kit.dart
@@ -3,5 +3,3 @@ export 'models/models.dart';
 export 'providers/providers.dart';
 export 'web/web.dart';
 export 'widgets/widgets.dart';
-
-typedef JsonMap = Map<String, dynamic>;
