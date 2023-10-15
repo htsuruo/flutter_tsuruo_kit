dfda0a8fff7ab12892ee50892a662819d435d4f8
htsuruo <tsuru.dev@gmail.com>
2023-10-15 11:56:36 +0900

    Rename to applyOrNull

--- lib/extensions/object.dart
+++ lib/extensions/object.dart
@@ -1,7 +1,7 @@
 extension ObjectX<T extends Object> on T? {
   // before: foo == null ? null : Text(foo)
   // after: foo.applyUnlessNull(Text.new)
-  V? applyUnlessNull<V>(V Function(T v) func) {
+  V? applyOrNull<V>(V Function(T v) func) {
     return this == null ? null : func(this!);
   }
 }
